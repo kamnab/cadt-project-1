ad73b0feaf4572657b972a814e4348e34f145f12
kamnab <k.kamnab@gmail.com>
2024-07-03 11:18:13 +0700

    .

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,12 @@ export default defineConfig({
     vue(),
   ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/shared/assets', import.meta.url)) },
+      { find: '@cmp', replacement: fileURLToPath(new URL('./src/shared/cmp', import.meta.url)) },
+      { find: '@stores', replacement: fileURLToPath(new URL('./src/shared/stores', import.meta.url)) },
+      { find: '@use', replacement: fileURLToPath(new URL('./src/shared/use', import.meta.url)) },
+    ],
   }
 })
